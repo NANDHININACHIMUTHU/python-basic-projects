80af371570b8ae4720092e6f930ce55560fd096f
NANDHINI NACHIMUTHU <108060752+NANDHININACHIMUTHU@users.noreply.github.com>
2026-01-12 16:17:01 +0530

    Create mind reading number game.py

---
+++ mini_projects/mind reading number game.py
@@ -0,0 +1,24 @@
+print("THINK OF ANY NUMBER IN YOUR MIND (Don’t tell me 😄)")
+
+input("Press Enter when ready...")
+
+print("➕ Add 10 to your number")
+input("Press Enter...")
+
+print("✖ Multiply the result by 2")
+input("Press Enter...")
+
+print("➖ Subtract 4 from the result")
+input("Press Enter...")
+
+print("➗ Divide the result by 2")
+input("Press Enter...")
+
+print("➕ Add 7 to the result")
+input("Press Enter...")
+
+total=int(input("Enter the final total value:"))
+number= total-15
+
+print("\n😲 Your final answer is:", number)
+print("How did I know your number? 😎")
